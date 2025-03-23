e5d249c76fc107b5652f1d67df98ea43680c3188
jmkn03 <demetrism2003@gmail.com>
2025-03-23 17:07:07 +0200

    fix importand changes

--- code/Database.sql
+++ code/Database.sql
@@ -1,18 +1,21 @@
+-- Active: 1742738056874@@127.0.0.1@3306@festivaldb
+-- Active: 1742738056874@@127.0.0.1@3306
 CREATE DATABASE festivalDB;
 
 CREATE TABLE guy(
     guy_id int NOT NULL AUTO_INCREMENT,
     name VARCHAR(255),
     email VARCHAR(255),
-    frog_id int,
-    PRIMARY KEY( guy_id ),
-    FOREIGN KEY( frog_id ) REFERENCES frogs(frog_id)
+    PRIMARY KEY( guy_id )
 )
 
 CREATE TABLE frogs(
     frog_id int NOT NULL AUTO_INCREMENT,
+    guy_id int NOT NULL,
     name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (frog_id)
+    PRIMARY KEY (frog_id),
+    FOREIGN KEY( guy_id ) REFERENCES guy(guy_id)
 )
 
-DROP TABLE Kati;
+DROP TABLE guy;
+DROP TABLE frogs;
