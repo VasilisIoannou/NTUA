6472c36afd5ccb472fd499b989460388812bafe5
jmkn03 <demetrism2003@gmail.com>
2025-05-10 22:00:58 +0300

    add view

--- code/Views.sql
+++ code/Views.sql
@@ -57,7 +57,7 @@ ORDER BY
 
 --2--
 
-/* View to find all the bands that belong to 'Rock' genre per year */
+/* View to find all the bands that belong to a given genre per year */
 CREATE OR REPLACE VIEW genre_bands_per_year AS
 SELECT DISTINCT
     f.festival_year,
@@ -154,5 +154,87 @@ GROUP BY
 ORDER BY
     lvl_exp ASC
 LIMIT 1;
+
+
+--8--
+
+
+
+
+--9--
+
+
+
+--10--
+
+
+--11--
+/* View to find all artists who have performed in 5 less festivals than the most performing artist */
+CREATE OR REPLACE VIEW artists_more_than_5_less_festivals_than_max AS
+WITH artist_festival_counts AS (
+    SELECT 
+        a.artist_id,
+        a.artist_name,
+        COUNT(DISTINCT f.festival_year) AS festivals_participated
+    FROM 
+        artist a
+    JOIN artist_band ab ON a.artist_id = ab.artist_id
+    JOIN band b ON ab.band_id = b.band_id
+    JOIN performance p ON p.band_id = b.band_id
+    JOIN event e ON e.event_id = p.event_id
+    JOIN festival f ON f.festival_year = e.festival_year
+    GROUP BY a.artist_id, a.artist_name
+),
+max_festival_count AS (
+    SELECT MAX(festivals_participated) AS max_festivals
+    FROM artist_festival_counts
+)
+SELECT 
+    afc.artist_id,
+    afc.artist_name,
+    afc.festivals_participated
+FROM 
+    artist_festival_counts afc
+JOIN 
+    max_festival_count mfc 
+    ON afc.festivals_participated < mfc.max_festivals - 5
+ORDER BY 
+    afc.festivals_participated DESC;
+
+
+--12--
+
+
+
+--13--
+CREATE OR REPLACE VIEW artists_in_3_or_more_continents AS
+SELECT 
+    a.artist_id,
+    a.artist_name,
+    COUNT(DISTINCT fl.continent) AS unique_continents
+FROM 
+    artist a
+JOIN artist_band ab ON a.artist_id = ab.artist_id
+JOIN band b ON ab.band_id = b.band_id
+JOIN performance p ON p.band_id = b.band_id
+JOIN event e ON e.event_id = p.event_id
+JOIN festival f ON f.festival_year = e.festival_year
+JOIN festival_location fl ON f.location_id = fl.location_id
+GROUP BY 
+    a.artist_id, a.artist_name
+HAVING 
+    COUNT(DISTINCT fl.continent) >= 3
+ORDER BY 
+    unique_continents DESC;
+
+
+--14--
+
+
+
+
+
+
+
 
 
